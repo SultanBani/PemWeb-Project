e4cc4e762a147229819b9eb2c1a19252abb717ad
Banii <sultant3hakim@gmail.com>
2025-06-03 17:49:11 +0700

    test

--- obatsaku_website/index.php
+++ obatsaku_website/index.php
@@ -17,10 +17,10 @@ include "db/koneksi.php";
         <ul>
             <li><a href="pages/dashboard.php">Dashboard</a></li>
             <li><a href="pages/kelola_obat.php">Kelola Obat</a></li>
-            <li><a href="pages/pesan_pengguna.php">Pesan Pengguna</a></li>
             <li><a href="pages/data_pesanan.php">Data Pesanan</a></li>
             <li><a href="pages/pembayaran.php">Pembayaran</a></li>
             <li><a href="pages/daftar_pengguna.php">Daftar Pengguna</a></li>
+            <li><a href="pages/pesan_pengguna.php">Hubungi Kami</a></li>
             <li><a href="logout.php">Logout</a></li>
         </ul>
     </div>
