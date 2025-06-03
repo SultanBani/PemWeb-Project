ce9dc956cecb5adcfd804bc76fdf8102e27e4cd8
vian-98 <rizki.favian123@gmail.com>
2025-06-03 13:05:15 +0700

    nambah fitur reset keranjang setelah cetak nota

--- obatsaku_website/pages/hapus_keranjang.php
+++ obatsaku_website/pages/hapus_keranjang.php
@@ -3,11 +3,13 @@ include "../db/koneksi.php";
 
 header('Content-Type: application/json');
 
-$sql = "TRUNCATE TABLE keranjang"; // Kosongkan tabel keranjang
-$result = mysqli_query($conn, $sql);
-
-if ($result) {
-    echo json_encode(['success' => true]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $hapus = mysqli_query($conn, "DELETE FROM keranjang");
+    if ($hapus) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Gagal menghapus data.']);
+    }
 } else {
-    echo json_encode(['success' => false]);
+    echo json_encode(['success' => false, 'message' => 'Metode tidak diizinkan']);
 }
