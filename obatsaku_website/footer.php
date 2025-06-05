c712a0e473ff59abfd684cc1a1d1f8c71504b338
rendyant <rendyant2305@gmail.com>
2025-06-05 09:24:40 +0700

    Update footer.php

---
+++ obatsaku_website/footer.php
@@ -0,0 +1,49 @@
+<!-- footer.php -->
+
+<footer>
+   <!-- Fav Icon -->
+    <link rel="shortcut icon" href="/assets/icon/favicon.ico" type="image/x-icon" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/dist/css/style.css" />
+    
+    <!-- Icon -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+
+  <div class="container">
+    <div class="footer-box">
+      <div class="box">
+        <h2>Social Media</h2>
+        <p>Obat Saku merupakan platform media sosial sekaligus katalog digital yang menyajikan informasi lengkap dan terpercaya seputar obat-obatan, mulai dari deskripsi, dosis, hingga efek samping, yang bertujuan untuk memudahkan masyarakat mengakses pengetahuan kesehatan secara cepat dan praktis.</p>
+        <div class="social">
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+          <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+          <a href="#"><i class="fa-brands fa-youtube"></i></a>
+        </div> 
+      </div>
+      <div class="box">
+        <h2>Menu</h2>
+        <a href="#">Beranda</a>
+        <a href="#">Katalog Obat</a>
+        <a href="#">Data Pesanan</a>
+        <a href="#">Hubungi Kami</a>
+      </div>
+      <div class="box">
+        <h2>Kontak</h2>
+        <p><i class="fa-solid fa-envelope"></i> obatsaku0406@gmail.com</p>
+        <p><i class="fa-solid fa-phone"></i>0856-2394-7689</p>
+      </div>
+    </div>
+
+    <div class="copyright">
+      <p>&copy; Copyright 2025 by <span>ObatSaku</span></p>
+    </div>
+  </div>
+</footer>
